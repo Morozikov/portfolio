54045b6a520f43f3f5c8a8b0744728caed4cb482
morozikov <stuffx@mail.ru>
2023-10-11 08:24:15 +0200

    add header example

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,27 +2,33 @@ import { Providers } from "@/providers";
 import "./globals.css";
 import type { Metadata } from "next";
 import { AppSidebar } from "@/components/AppSidebar";
+import { AppHeader } from "@/components/AppHeader";
 
 export const metadata: Metadata = {
-  title: "Morozikov Vitalii",
-  description: "Portfolio of Morozikov Vitalii",
+	title: "Morozikov Vitalii",
+	description: "Portfolio of Morozikov Vitalii",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en" className="dark">
-      <body>
-        <Providers>
-          <div className="flex flex-row">
-            <AppSidebar />
-            {children}
-          </div>
-        </Providers>
-      </body>
-    </html>
-  );
+	return (
+		<html
+			lang="en"
+			className="dark">
+			<body className="container">
+				<Providers>
+					<div className="flex flex-row">
+						<AppSidebar />
+						<div className="flex flex-col w-full p-2">
+							<AppHeader />
+							<main>{children}</main>
+						</div>
+					</div>
+				</Providers>
+			</body>
+		</html>
+	);
 }
---
+++ components/AppHeader.tsx
@@ -0,0 +1,7 @@
+export const AppHeader = () => {
+	return (
+		<div>
+			<h1>Header</h1>
+		</div>
+	);
+};
